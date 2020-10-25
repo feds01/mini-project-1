3146e3254c070d3b00786d96d8211ce931000811
Alex <alexander.fedotov.uk@gmail.com>
2020-10-25 21:21:46 +0000

    Moved IEntry interface to src/interfaces

---
+++ src/interfaces/IEntry.java
@@ -0,0 +1,28 @@
+package interfaces;
+
+import common.resources.EntryType;
+
+import java.nio.file.Path;
+
+/**
+ * Interface that represents either a Directory of File entry when the file
+ * system lists the folders contents.
+ *
+ * @author 20008575
+ * */
+public interface IEntry {
+    /**
+     * Method that is used to return the type of entry, whether it's a file or
+     * a directory.
+     *
+     * @return A {@link EntryType}, representing if it's a file or a directory.
+     * */
+    EntryType getType();
+
+    /**
+     * Method to get the path of a file system entry.
+     *
+     * @return A {@link Path} that points to the entry resource.
+     * */
+    Path getPath();
+}
